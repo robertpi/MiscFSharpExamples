6129a6f0fdc114c637de0e6e13f750550a4d1ef7
Robert Pickering <robert@strangelights.com>
2010-08-01 17:07:50 +0200

    first commit!

---
+++ FsLexYaccExample/FsLexYaccExample/ast.fs
@@ -0,0 +1,20 @@
+﻿// Parser generators typically produce numbers represented by values in an F# [Algebraic Data Type].  For example, the following is taken (with permission) from the F# sample samples\fsharp\Parsing: 
+module Ast
+
+type expr = 
+| Val of string 
+| Int of int
+| Float of float
+| Decr of expr
+ 
+type stmt = 
+| Assign of string * expr
+| While of expr * stmt
+| Seq of stmt list
+| IfThen of expr * stmt
+| IfThenElse of expr * stmt * stmt
+| Print of expr
+ 
+type prog = Prog of stmt list
+
+
