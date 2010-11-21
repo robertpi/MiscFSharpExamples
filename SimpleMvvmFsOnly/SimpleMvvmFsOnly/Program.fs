dfc977ae5942b189d9a79b28fd7dda6553062d14
Robert Pickering <robert@strangelights.com>
2010-11-21 13:14:57 +0100

    A MVVM example with F# only

---
+++ SimpleMvvmFsOnly/SimpleMvvmFsOnly/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.Reflection
+open System.Windows
+open System.Windows.Markup
+open Strangelights.FsViewModels
+
+let mainWindowStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MainWindow.xaml")
+let mainWindow = 
+    let win = XamlReader.Load(mainWindowStream) :?> Window
+    let viewModel = new StockViewerViewModel()
+    win.DataContext <- viewModel
+    win.Loaded.Add(fun _ -> viewModel.OnLoaded())
+    win
+
+let app = new Application()
+
+[<STAThread>]
+app.Run mainWindow |> ignore
