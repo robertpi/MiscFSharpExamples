77feaffe628997bec5e8c72a41ea56ab7558cac9
Robert Pickering <robert@strangelights.com>
2010-11-22 09:54:48 +0100

    yet more comments

--- SimpleMvvmFsOnly/SimpleMvvmFsOnly/StockViewerViewModel.fs
+++ SimpleMvvmFsOnly/SimpleMvvmFsOnly/StockViewerViewModel.fs
@@ -56,6 +56,7 @@ type Quote =
     { Date: DateTime
       Value: float }
 
+// A view model for a simple stock price history viewing app
 type StockViewerViewModel() =
     // indicates if the screen is busy or not
     let mutable isBusy = true
